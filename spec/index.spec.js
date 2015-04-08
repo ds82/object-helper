ff5b57106bccba7266971ab420e6e49eea1a8a34
Dennis Sänger <mail@dennis.io>
2015-04-08 11:38:18 +0200

    test(objectHelper.set): add basic test for set()

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -4,18 +4,27 @@ var UUT = require('../dist');
 
 describe('object-helper', function() {
 
+  var uut, stubObject;
+
+  beforeEach(function() {
+    stubObject = {};
+    uut = new UUT(stubObject);
+  });
+
   describe('get', function() {
-
-    var uut, stubObject;
-
-    beforeEach(function() {
-      stubObject = {};
-      uut = new UUT(stubObject);
-    });
 
     it('should return correct value in object', function() {
       stubObject.a = {b: {c: 1}};
       expect(uut.get('a.b.c')).toBe(1);
+    });
+
+  });
+
+  describe('set', function() {
+
+    it('should set the value of an object', function() {
+      uut.set('x.y.z', 10);
+      expect(stubObject.x.y.z).toBe(10);
     });
 
   });
